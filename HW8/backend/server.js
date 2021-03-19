780ca6fd92781218b01192410d83f0448526c884
hongyulong <hongyulong1224@gmail.com>
2021-03-19 15:40:36 +0800

    dev framework

--- HW8/backend/server.js
+++ HW8/backend/server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
     res.send('Hello World!');
 })
 
+
+app.get('/api/movie', function(req, res){
+    res.send('Movie data!');
+})
 var server = app.listen(8080, function() {
     console.log('Backend Application listening at http://localhost:8080');
 })
