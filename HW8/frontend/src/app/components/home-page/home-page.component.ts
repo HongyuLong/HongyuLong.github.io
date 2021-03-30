44dfca7a6879ac81cd929c3281475c521847c3e3
hongyulong <hongyulong1224@gmail.com>
2021-03-30 15:14:45 +0800

    home page nearly finished

--- HW8/frontend/src/app/components/home-page/home-page.component.ts
+++ HW8/frontend/src/app/components/home-page/home-page.component.ts
@@ -9,11 +9,24 @@ import {DataService} from '../../data.service'
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  //all_data = {}
   now_playing: any;
   pop_mv_single: any;
   pop_mv_grouped: any;
+
+  top_mv_single: any;
+  top_mv_grouped: any;
+
+  trending_mv_single: any;
+  trending_mv_grouped: any;
+
+  pop_tv_single: any;
+  pop_tv_grouped: any;
+
+  top_tv_single: any;
+  top_tv_grouped: any;
+
+  trending_tv_single: any;
+  trending_tv_grouped: any;
 
   mobile: boolean=false;
 
@@ -26,6 +39,21 @@ export class HomePageComponent implements OnInit {
       this.now_playing = data.now_playing;
       this.pop_mv_single = data.pop_mv.single;
       this.pop_mv_grouped = data.pop_mv.grouped;
+
+      this.top_mv_single = data.top_mv.single;
+      this.top_mv_grouped = data.top_mv.grouped;
+
+      this.trending_mv_single = data.trending_mv.single;
+      this.trending_mv_grouped = data.trending_mv.grouped;
+
+      this.pop_tv_single = data.pop_tv.single;
+      this.pop_tv_grouped = data.pop_tv.grouped;
+
+      this.top_tv_single = data.top_tv.single;
+      this.top_tv_grouped = data.top_tv.grouped;
+
+      this.trending_tv_single = data.trending_tv.single;
+      this.trending_tv_grouped = data.trending_tv.grouped;
     }) 
     if (window.screen.width === 360) { // 768px portrait
       this.mobile = true;
